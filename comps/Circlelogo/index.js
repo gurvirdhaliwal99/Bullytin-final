2df9249668303e587f6603eadcaca4d90b8748eb
willusmac <wmcdonald26@my.bcit.ca>
2021-04-08 00:12:48 -0700

    Circle logo fixed (centered)

--- comps/Circlelogo/index.js
+++ comps/Circlelogo/index.js
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 
 const LogoCircle = styled.div`
     display: flex;
-    justify-content: center;
-    align-items: center;
     background-color:#e5e5e5;
     background-image:url("Logo.png");
+    background-position: center;
     background-repeat: no-repeat;
     height: 75px;
     width: 75px;
