f0a35bb79e091502b27a2a1d287bcda12e7e70f9
willusmac <wmcdonald26@my.bcit.ca>
2021-05-18 14:47:34 -0700

    Back button changed to a react icon

--- comps/Backbutton/index.js
+++ comps/Backbutton/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import {useRouter} from "next/router";
-
+import {IoArrowBackCircleOutline} from 'react-icons/io5';
 const BackButton = styled.div`
     display: flex;
     background-position: center;
-    height: 60px;
-    width: 60px;
+    height: 100%;
+    width: 100%;
     position: relative;
     left: ${props=>props.left};
     top: ${props=>props.top};
+
+        .back{
+            font-size: 4em;
+        }
 `;
 
 
@@ -22,7 +26,8 @@ const Back = ({
     const router = useRouter();
     return <div>
         <BackButton onClick={()=>router.push(routerBack)} top={top} left={left}>
-            <img src="../back.png"/>
+            {/* <img src="../back.png"/> */}
+            <IoArrowBackCircleOutline className="back"/>
         </BackButton>
     </div>
 }
